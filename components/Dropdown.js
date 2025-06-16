a7d5cbab55f0299b72ee161efb4f31b27b0d6c08
MaltheRiis <maltheriis@hotmail.com>
2025-06-16 15:36:30 +0200

    Update Dropdown.js

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,14 +1,16 @@
+// components/Dropdown.js
 import React from 'react';
 
 const Dropdown = ({ selectedSeason, onChange }) => {
   return (
-    <select value={selectedSeason} onChange={e => onChange(e.target.value)}>
-      <option value="Sæson2">Sæson 2</option>
-      <option value="Sæson3">Sæson 3</option>
-      <option value="Sæson4">Sæson 4</option>
-    </select>
+    <div className="dropdown">
+      <select value={selectedSeason} onChange={(e) => onChange(e.target.value)}>
+        <option value="Sæson2">Sæson 2</option>
+        <option value="Sæson3">Sæson 3</option>
+        <option value="Sæson4">Sæson 4</option>
+      </select>
+    </div>
   );
 };
 
 export default Dropdown;
-
